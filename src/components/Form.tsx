5295990e408792f78ac72b0d5b9ad953a2b9a5fe
Szymon Nawrocki <snawrocki@fandom.com>
2022-06-27 21:56:50 +0200

    Add new Form component

---
+++ src/components/Form.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+import './Form.css';
+import {TodoInterface} from "../todos";
+
+interface Props {
+    visible: boolean;
+    todos: TodoInterface[];
+    setTodos: (todos: TodoInterface[]) => void;
+}
+
+const Form = ({ visible, todos, setTodos }: Props) => {
+    const [todo, setTodo] = useState('');
+
+    if (!visible) {
+        return null;
+    }
+
+    const addTodo = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const newTodo: TodoInterface = {
+            id: Math.random().toString(16).slice(2),
+            name: todo,
+            done: false,
+        };
+
+        setTodo('');
+        setTodos(todos.concat(newTodo));
+    };
+
+    return (
+        <form className="list-form" onSubmit={addTodo}>
+            <input
+                placeholder="Add new todo"
+                value={todo}
+                onChange={(e) => setTodo(e.target.value)}
+            />
+        </form>
+    );
+};
+
+export default Form;
