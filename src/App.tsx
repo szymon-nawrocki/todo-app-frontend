1556352cb27b92d2471faa8927f64d632f87beec
Szymon Nawrocki <snawrocki@fandom.com>
2022-06-28 22:44:04 +0200

    Move background to div wrapper element

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ import './App.css';
 import List from './components/List';
 
 const App = () => (
-    <div className="app-container">
-        <List />
+    <div className="app-background">
+        <div className="app-container">
+            <List />
+        </div>
     </div>
 );
 
